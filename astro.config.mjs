58733eb9b1772e4388f886c92be8ddff0dd48128
Flavio Ruiz <flavio.r.1200@gmail.com>
2023-04-26 19:57:31 -0400

    listo para lanzar en gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://Flavio1200.github.io',
+  base: '/practica-astro',
 });
